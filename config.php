7ad8769ea78351c90fd8d1a45888a888053c60f3
Ezra Kuhr <Ezra.Kuhr@gmail.com>
2021-07-09 06:28:45 -0400

    adding laravel packages

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-static $mysql_hostname = "";
-static $mysql_username = "";
-static $mysql_password = "";
-static $mysql_database = "";
-static $subscribe_keyword = "";
-static $unsubscribe_keyword = "";
-static $twilio_account_sid = "";
-static $twilio_auth_token = "";
---
+++ legacyapp/config.php
@@ -0,0 +1,9 @@
+<?php
+static $mysql_hostname = "localhost";
+static $mysql_username = "sub";
+static $mysql_password = "sub";
+static $mysql_database = "sub";
+static $subscribe_keyword = "SUB";
+static $unsubscribe_keyword = "UNSUB";
+static $twilio_account_sid = "AC132a766dbb549f2fca4a629b23809d17";
+static $twilio_auth_token = "6da411e99ccec063006d666b2da9b1b8";
